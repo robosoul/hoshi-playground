b5c9e93d8e1f4303b607aaa658717d6f0d2d517c
robosoul <obradovic.luka.83@gmail.com>
2015-05-24 12:01:40 +0200

    minor licence formatting

--- src/main/java/org/hoshi/playground/collections/ListUtils.java
+++ src/main/java/org/hoshi/playground/collections/ListUtils.java
@@ -1,18 +1,18 @@
 /**
  * The MIT License (MIT)
- * <p/>
+ *
  * Copyright (C) 2015 Luka Obradovic.
- * <p/>
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * <p/>
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * <p/>
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -23,9 +23,6 @@
  */
 package org.hoshi.playground.collections;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.List;
@@ -35,8 +32,6 @@ import java.util.Set;
  * @author Luka Obradovic (obradovic.luka.83@gmail.com)
  */
 public final class ListUtils {
-    public static final Logger log = LoggerFactory.getLogger(ListUtils.class);
-
     private ListUtils() {
         // utility class
     }
